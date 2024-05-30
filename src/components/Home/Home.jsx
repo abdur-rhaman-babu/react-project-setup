394b6862cf38cafc99c22952f09f3a1abf44d913
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-05-30 23:59:10 +0600

    show all data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import Jobs from "../Jobs/Jobs";
 const Home = () => {
     const [jobs, setJobs] = useState([])
     // console.log(jobs)
+    const [jobslength, setJobLength] = useState(4)
     useEffect(()=>{
         fetch('jobdata.json')
         .then((res)=>res.json())
@@ -11,11 +12,16 @@ const Home = () => {
     },[])
 
     return (
-        <div className="lg:grid grid-cols-2 mx-auto max-w-5xl gap-4 mt-24">
+       <div>
+             <div className="lg:grid grid-cols-2 mx-auto max-w-5xl gap-4 mt-24">
             {
-                jobs &&  jobs.slice(0,4).map((job)=><Jobs key={job.id} jobs = {job}/>)
+                jobs &&  jobs.slice(0,jobslength).map((job)=><Jobs key={job.id} jobs = {job}/>)
             }
         </div>
+           <div className= {jobslength === jobs.length ? 'hidden': 'text-center mt-3'}>
+           <button onClick={()=>setJobLength(jobs.length)} className="btn btn-primary">All Jobs</button>
+           </div>
+       </div>
     );
 };
 
